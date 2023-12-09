c2d7dce999efb16927ce003281bb7bf530f19be0
Orlando Tsuji <119526931+orlando-tsuji@users.noreply.github.com>
2023-12-09 14:34:16 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 
 // Data final da contagem regressiva (pode ser ajustada conforme necessário)
-const dataFinal = new Date('2023-12-08T21:30:00');
-const dataHenrique = new Date('2023-12-08T23:00:00');
+const dataFinal = new Date('2023-12-09T21:30:00');
+// const dataHenrique = new Date('2023-12-08T23:00:00');
 
 const contagemRegressiva = document.querySelector('#contagem');
-const contagemRegressivaH = document.querySelector('#contagemH');
+// const contagemRegressivaH = document.querySelector('#contagemH');
 
 
 function formatTwoDigits(number) {
@@ -14,28 +14,28 @@ function formatTwoDigits(number) {
 function atualizaContagemRegressiva() {
     const agora = new Date();
     const diferenca = dataFinal - agora;
-    const diferencaH = dataHenrique - agora;
+    // const diferencaH = dataHenrique - agora;
 
     // Calcular horas, minutos, segundos
     const horas = formatTwoDigits(Math.floor(diferenca / (1000 * 60 * 60)));
     const minutos = formatTwoDigits(Math.floor((diferenca % (1000 * 60 * 60)) / (1000 * 60)));
     const segundos = formatTwoDigits(Math.floor((diferenca % (1000 * 60)) / 1000));
 
-    const horasH = formatTwoDigits(Math.floor(diferencaH / (1000 * 60 * 60)));
-    const minutosH = formatTwoDigits(Math.floor((diferencaH % (1000 * 60 * 60)) / (1000 * 60)));
-    const segundosH = formatTwoDigits(Math.floor((diferencaH % (1000 * 60)) / 1000));
+    // const horasH = formatTwoDigits(Math.floor(diferencaH / (1000 * 60 * 60)));
+    // const minutosH = formatTwoDigits(Math.floor((diferencaH % (1000 * 60 * 60)) / (1000 * 60)));
+    // const segundosH = formatTwoDigits(Math.floor((diferencaH % (1000 * 60)) / 1000));
 
     
 
     // Atualizar o elemento HTML
     contagemRegressiva.textContent = `${horas}:${minutos}:${segundos}`;
-    contagemRegressivaH.textContent = `${horasH}:${minutosH}:${segundosH} HENRIQUE`;
+    // contagemRegressivaH.textContent = `${horasH}:${minutosH}:${segundosH} HENRIQUE`;
 
     
     // Se a contagem regressiva terminou, exibir uma mensagem
     if (diferenca < 0) {
-        contagemRegressiva.textContent = 'A contagem regressiva terminou!';
-        contagemRegressivah.textContent = 'A contagem regressiva terminou!';
+        contagemRegressiva.textContent = BORA JOGAR!!!';
+        // contagemRegressivah.textContent = 'A contagem regressiva terminou!';
     }
 }
 
