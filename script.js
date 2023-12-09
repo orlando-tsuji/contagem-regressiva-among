b82f5351e4bbe549adb84e16bb1430eebf90968c
Orlando Tsuji <119526931+orlando-tsuji@users.noreply.github.com>
2023-12-08 21:22:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 
 // Data final da contagem regressiva (pode ser ajustada conforme necessário)
 const dataFinal = new Date('2023-12-08T21:30:00');
+const dataHenrique = new Date('2023-12-08T23:00:00');
 
 const contagemRegressiva = document.querySelector('#contagem');
+const contagemRegressivaH = document.querySelector('#contagemH');
+
 
 function formatTwoDigits(number) {
     return number < 10 ? `0${number}` : number;
@@ -11,18 +14,28 @@ function formatTwoDigits(number) {
 function atualizaContagemRegressiva() {
     const agora = new Date();
     const diferenca = dataFinal - agora;
+    const diferencaH = dataHenrique - agora;
 
     // Calcular horas, minutos, segundos
     const horas = formatTwoDigits(Math.floor(diferenca / (1000 * 60 * 60)));
     const minutos = formatTwoDigits(Math.floor((diferenca % (1000 * 60 * 60)) / (1000 * 60)));
     const segundos = formatTwoDigits(Math.floor((diferenca % (1000 * 60)) / 1000));
 
+    const horasH = formatTwoDigits(Math.floor(diferencaH / (1000 * 60 * 60)));
+    const minutosH = formatTwoDigits(Math.floor((diferencaH % (1000 * 60 * 60)) / (1000 * 60)));
+    const segundosH = formatTwoDigits(Math.floor((diferencaH % (1000 * 60)) / 1000));
+
+    
+
     // Atualizar o elemento HTML
     contagemRegressiva.textContent = `${horas}:${minutos}:${segundos}`;
+    contagemRegressivaH.textContent = `${horasH}:${minutosH}:${segundosH} HENRIQUE`;
 
+    
     // Se a contagem regressiva terminou, exibir uma mensagem
     if (diferenca < 0) {
-        contagemRegressiva.textContent = 'A contagem regressiva terminou!';
+        contagemRegressiva.textContent = '<h2>A contagem regressiva terminou!<h2>';
+        contagemRegressivah.textContent = 'A contagem regressiva terminou!';
     }
 }
 
